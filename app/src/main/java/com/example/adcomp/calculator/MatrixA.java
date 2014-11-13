3571a48080465b84606bd729154edd6cd2b3889d
Adwait Nadkarni <adwait1992@gmail.com>
2014-11-12 21:31:03 -0800

    some more minor changes
    nothing special to report here

--- app/src/main/java/com/example/adcomp/calculator/MatrixA.java
+++ app/src/main/java/com/example/adcomp/calculator/MatrixA.java
@@ -259,11 +259,6 @@ public class MatrixA extends Activity {
         }
     }
 
-
-
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
